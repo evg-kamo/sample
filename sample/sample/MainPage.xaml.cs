188364af8ba6494fac7d77491962a079b0651939
kamo <kamo@ever-g.co.jp>
2021-11-16 09:06:22 +0900

    プロジェクト ファイルを追加します。

---
+++ sample/sample/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+
+        // 登録ボタン押下
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text))
+            {
+                await App.Database.SavePersonAsync(new Person
+                {
+                    Name = nameEntry.Text
+                });
+
+                nameEntry.Text = string.Empty;
+                collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+            }
+        }
+
+        // カレンダーアイコン押下
+        async void Ondisplaycalenderclicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("カレンダー", "This is Calender!!!", "OK");
+        }
+
+        // カメラアイコン押下
+        async void Ondisplaycameraclicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("カメラ", "これはカメラです!!!", "OK");
+        }
+
+        // 貯金箱アイコン押下
+        async void Ondisplaymoneyclicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("貯金箱", "이것은 돼지 저금통!!!", "OK");
+        }
+
+        // 設定アイコン押下
+        async void Ondisplaysettingclicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("設定", "这是设置画面!!!", "OK");
+        }
+    }
+}
---
+++ sample/sample/Person.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace sample
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
